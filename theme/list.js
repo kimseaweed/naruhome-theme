54a6f9c557316ab02e4919d49724c8b4ace1fdef
김자반 <leeminji1944@gmail.com>
2025-07-13 20:03:52 +0900

    Create list.js

---
+++ theme/list.js
@@ -0,0 +1,8 @@
+const themeList = [
+  {name: '기본 라이트', code: 'default-light'},
+  {name: '기본 다크', code: 'default-light'},
+  {name: '기본 베이지', code: 'default-light'},
+  {name: '기본 블루', code: 'default-light'},
+];
+
+export default themeList
